730e910f13f1dad2ada525b5c166f75f82575178
Ryan <110856280+rythedino@users.noreply.github.com>
2022-08-10 19:23:03 -0400

    Add files via upload

---
+++ BankHeist.cs
@@ -0,0 +1,24 @@
+﻿using LSPD_First_Response.Mod.API;
+using Rage;
+using System;
+
+namespace Bank_Robbery_Callout
+{
+    public class BankHeist : Plugin
+    {
+        public override void Finally()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegisterCallouts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using LSPD_First_Response.Mod.API;  //Reference LSPDFR
+
+using Rage; //Reference RPH
+namespace Bank_Robbery_Callout
+{
+    public class Main : Plugin
+    {
+        public static String Version = "1.0.0";
+
+        private static void OnOnDutyStateChangedHandler(bool OnDuty)
+
+        { }
+
+        public override void Finally()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegisterCallouts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
